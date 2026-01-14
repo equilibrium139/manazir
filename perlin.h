f3a2b68b9a1019af079d28ba1901ffc04675be6f
Zaid Al-ruwaishan <zaidruwaishan@gmail.com>
2026-01-14 17:09:16 -0500

    smooth perlin

--- perlin.h
+++ perlin.h
@@ -16,11 +16,28 @@ public:
     }
 
     float Noise(const glm::vec3& p) const {
-        int i = int(4 * p.x) & 255;
-        int j = int(4 * p.y) & 255;
-        int k = int(4 * p.z) & 255;
+        float u = p.x - std::floor(p.x);
+        float v = p.y - std::floor(p.y);
+        float w = p.z - std::floor(p.z);
 
-        return randFloats[permX[i] ^ permY[j] ^ permZ[k]];
+        int i = int(std::floor(p.x));
+        int j = int(std::floor(p.y));
+        int k = int(std::floor(p.z));
+
+        float c[2][2][2];
+        for (int di = 0; di < 2; di++) {
+            for (int dj = 0; dj < 2; dj++) {
+                for (int dk = 0; dk < 2; dk++) {
+                    c[di][dj][dk] = randFloats[
+                        permX[(i+di) & 255] ^
+                        permY[(j+dj) & 255] ^
+                        permZ[(k+dk) & 255]
+                    ];
+                }
+            }
+        }
+
+        return TrilinearInterp(c, u, v, w);
     }
 private:
     static const int pointCount = 256;
@@ -34,5 +51,20 @@ private:
             p[i] = i;
         }
         std::random_shuffle(p, p + pointCount);
+    }
+
+    static float TrilinearInterp(float c[2][2][2], float u, float v, float w) {
+        float accum = 0.0f;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    accum += (i*u + (1-i)*(1-u))
+                            *(j*v + (1-j)*(1-v))
+                            *(k*w + (1-k)*(1-w))
+                            *c[i][j][k];
+                }
+            }
+        }
+        return accum;
     }
 };
